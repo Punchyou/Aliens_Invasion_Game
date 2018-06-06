f117d7461e9128e9b312691d52e2b012ba1dbdd2
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2018-06-06 23:52:41 +0300

    Initial Project Version

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    # A class that includes all settings for Aliens Invasion
+
+    def __init__(self):
+        #initialize game's settings
+        self.screen_width = 1700
+        self.screen_height = 700
+        self.bg_color = (120, 50, 250)
